2b1a5592a5b854caf9d58e65aa3db433f031ca34
falcon <wuzhangjin@gmail.com>
2017-09-16 22:16:48 +0800

    tools: build.sh: document more
    Signed-off-by: falcon <wuzhangjin@gmail.com>

--- src/tools/build.sh
+++ src/tools/build.sh
@@ -16,11 +16,11 @@ SYS_SIZE=$((RAMDISK_START * 1024))
 [ ! -f "$bootsect" ] && echo "Error: No bootsect binary file there" && exit -1
 dd if=$bootsect bs=512 count=1 of=$IMAGE >/dev/null 2>&1
 
-# Write setup (4 * 512bytes, 4 sectors)
+# Write setup (4 * 512bytes, 4 sectors) see SETUPLEN in boot/bootsect.s
 [ ! -f "$setup" ] && echo "Error: No setup binary file there" && exit -1
 dd if=$setup seek=1 bs=512 count=4 of=$IMAGE >/dev/null 2>&1
 
-# Write kernel (< SYS_SIZE)
+# Write kernel (< SYS_SIZE), see the hardcoded SYSSIZE in src/boot/bootsetc.s
 [ ! -f "$kernel" ] && echo "Error: No kernel binary file there" && exit -1
 
 kernel_size=`wc -c $kernel | tr -C -d [0-9]`

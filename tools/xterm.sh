5723a41a1dce8a45a4f9d1849b57a03374f5a99a
falcon <wuzhangjin@gmail.com>
2017-10-18 22:16:48 +0800

    xterm.sh: clean up the function name
    Signed-off-by: falcon <wuzhangjin@gmail.com>

--- tools/xterm.sh
+++ tools/xterm.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 ppid () { ps -p ${1:-$$} -o ppid=; }
-pcom () { ps -p ${1:-$$} -o cmd=; }
+pcmd () { ps -p ${1:-$$} -o cmd=; }
 pppid () { ppid `ppid ${1:-$$}`; }
-ppcom () { pcom `pppid ${1:-$$}`; }
+ppcmd () { pcmd `pppid ${1:-$$}`; }
 ppppid () { pppid `ppid ${1:-$$}`; }
-pppcom () { pcom `ppppid ${1:-$$}`; }
+pppcmd () { pcmd `ppppid ${1:-$$}`; }
 pppppid () { ppppid `ppid ${1:-$$}`; }
-ppppcom () { pcom `pppppid ${1:-$$}`; }
+ppppcmd () { pcmd `pppppid ${1:-$$}`; }
 
 # Override XTERM
-_XTERM=`ppppcom`
+_XTERM=`ppppcmd`
 echo $_XTERM | grep -q 'term'
 [ $? -eq 0 ] && XTERM=$(basename `echo $_XTERM | tr ' ' '\n' | grep 'term'`)
 
